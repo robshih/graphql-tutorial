b78548631bcf20cb471b4a362879510946db4864
Rob Shih <robshih33@gmail.com>
2020-04-13 16:14:15 +0100

    Added back controller annotation that was accidentally removed

--- src/main/java/io/robshih/graphqltutorial/controller/GraphQLController.java
+++ src/main/java/io/robshih/graphqltutorial/controller/GraphQLController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
 @RequestMapping("/graphql")
 class GraphQLController {
 
@@ -14,11 +18,12 @@ class GraphQLController {
     private GraphQL graphQl;
 
     @GetMapping
-    public Object get(@RequestParam("query") String query) {
+    public Map<String, Object> get(@RequestParam("query") String query) {
         return graphQl
                 .execute(ExecutionInput
                         .newExecutionInput(query)
                         .build())
                 .toSpecification();
     }
+
 }
